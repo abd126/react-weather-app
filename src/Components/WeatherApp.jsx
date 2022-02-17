f1d4acff76a69716fcdd8e62add7d60f9c187e7d
Abdullah <abdullahusman126@gmail.com>
2022-02-18 01:42:31 +0500

    Complete

--- src/Components/WeatherApp.jsx
+++ src/Components/WeatherApp.jsx
@@ -1,70 +1,108 @@
-import React, { useEffect,useState } from 'react'
-
+import React, { useEffect, useState } from 'react'
+import './style.css'
+import { Search , BsFillGeoAltFill } from 'react-bootstrap-icons';
+import {ImLocation} from 'react-icons/im'
 
 
 
 const api = {
   key: "56a9c1b59e94a977720820c838c997b7",
   base: "https://api.openweathermap.org/data/2.5/",
-  units:"metric",
+  units: "metric",
 
 }
 
 const WeatherApp = () => {
-  const [weatherData , setWeatherData] = useState({})
-  const [cityName , setCityname] = useState("")
-  const [searchCityName , setSearchCityName] = useState("")
-  
-  
-  
-  useEffect(()=>{
-      fetch(`${api.base}forecast?q=${searchCityName}&appid=${api.key}&units=${api.units}&cnt=3`)
-    .then((res)=>res.json())
-    .then((result)=>{
-      setWeatherData(result)
-    }).catch((err)=>{
+  const [weatherData, setWeatherData] = useState({})
+  const [cityName, setCityname] = useState("")
+  const [searchCityName, setSearchCityName] = useState("Karachi")
+
+
+
+  useEffect(() => {
+    fetch(`${api.base}weather?q=${searchCityName}&appid=${api.key}&units=${api.units}&cnt=3`)
+      .then((res) => res.json())
+      .then((result) => {
+        setWeatherData(result)
+      }).catch((err) => {
         console.log(err)
-    })
+      })
 
-  },[searchCityName])
+  }, [searchCityName])
   console.log(weatherData)
-    
-  // console.log(weatherData)
-    //Search Function 
-    const searchCity = (e) =>{
-      setSearchCityName(cityName)
-      // console.log("City Name",weatherData.city.name)
-      // console.log("City Temp",weatherData.list.main)
+  //Search Function 
+  const searchCity = (e) => {
+    setSearchCityName(cityName)
+
+  }
 
 
-    }
+  return (
+    <div className='Main container'>
+      <h1>Weather App</h1>
+      <div className='Search-component'>
+        <div className='d-flex justify-content-center my-5'>
+
+          <input type="text"
+            value={cityName}
+            placeholder="Search City Name"
+            onChange={(e) => setCityname(e.target.value)}
+            className="w-75"
+          />
+
+          <button className='btn btn-info'
+            onClick={searchCity}>
+            <Search />
+          </button>
+        </div>
+      </div>
+
+      <div className='detail-component'>
+
+        <div className='Temp-content'>
+
+          <h2 className=''>
+            <ImLocation />
+            {weatherData && weatherData.name}</h2>
 
 
-    return (
-    <div className='Main'>
-      <h1>Weather App</h1>        
-      <div className='d-flex justify-content-center my-5'>
-        <input type="text" 
-        value={cityName}
-        placeholder="Search City Name"
-        onChange={(e)=>setCityname(e.target.value)}
-        />
-      <button className='btn btn-info'
-      onClick={searchCity}>
-        Search
-      </button>
+          <div className="temprature">
+
+            {weatherData &&
+              weatherData.weather &&
+              weatherData.weather[0] &&
+              <p>{weatherData.weather[0].main}</p>
+              &&
+              <img className="city-icon"
+                src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+                alt={weatherData.weather[0].main} />
+            }
+
+            {weatherData &&
+              weatherData.weather &&
+              weatherData.weather[0] &&
+              <p>{weatherData.weather[0].main}</p>
+            }
+          </div>
+            <div className='temprature-reading'>
+            <h1 className='temprature-heading'>{weatherData && 
+          weatherData.main && weatherData.main.temp}</h1>
+          <span className='symbol'>°C</span>
+            </div>
+          <h3>
+            feels Like 
+          {weatherData &&
+              weatherData.main &&
+              weatherData.main.feels_like}
+          </h3>
+        </div>
+
 
       </div>
 
-      {/* <li>City : {weatherData && weatherData.name}</li>
-      <li>Temp : {weatherData && weatherData.main && weatherData.main.temp}</li>
-      <li>
-        {weatherData &&
-        weatherData.weather &&
-        weatherData.weather[0] &&
-        weatherData.weather[0].main}
-      </li> */}
-      {/* <p>{weatherData && weatherData.list[main.temp]}</p> */}
+
+
+
     </div>
   )
 }
